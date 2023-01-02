b2f21c31e5b5a8d70cd83f9a1a72df0d2e418642
dolsen0 <derek.olsen@gmail.com>
2023-01-02 13:13:25 -0500

    added ascii art generation from user prompt

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ from art import *
 coffee = art("coffee")
 random = randart()
 hello = tprint("hello")
+
+# prints user prompt into ascii
 prompt = input('What would you like to say?')
 message = tprint(prompt)
 
-print(message)
+#prints ascii art from user selected subject
+subject = input("what would you like to see as ascii art?")
+artSubject = art(subject)
+
+print(artSubject)
