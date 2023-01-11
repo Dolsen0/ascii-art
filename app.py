c5f8610cfa76e63dae8b0561153a5da674eaf39c
dolsen0 <derek.olsen@gmail.com>
2023-01-10 23:55:25 -0500

    cleaned up code

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from art import *
 
-coffee = art("coffee")
 random = randart()
 hello = tprint("hello")
 
-# prints user prompt into ascii
 prompt = input('What would you like to say?')
 message = tprint(prompt)
 
-#prints ascii art from user selected subject
 subject = input("what would you like to see as ascii art?")
 artSubject = art(subject)
 
 print(artSubject)
+
